6da9f95dc0bacd7fa39190180320b21dced9d047
Hiremata Rohith <rohi29717@gmail.com>
2026-06-14 09:18:19 +0530

    Remove broken frontend submodule and track frontend as normal directory

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -45,6 +45,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={`${nunito.variable} ${fredoka.variable}`}>
       <head>
+        <link rel="icon" href="/favicon.svg" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
